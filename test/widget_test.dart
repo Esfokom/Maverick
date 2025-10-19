4525173fb529b5ce35520433d9b1674ebb9fc4eb
Elvis Fosu Owusu <esfokom@gmail.com>
2025-10-19 11:09:14 +0000

    Fix merge conflicts and update project metadata to reflect correct application name and description

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-<<<<<<< HEAD
 import 'package:maverick/main.dart';
-=======
-import 'package:newapp/main.dart';
->>>>>>> 6e52787 (Added SignIn Page, SignUp page , Home screen)
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
